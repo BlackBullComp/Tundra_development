d3adf16b5c3b71f86d18266efcb5216cf0ba71d0
yuso1 <yusufilkis9@gmail.com>
2023-02-12 16:46:35 +0300

    hungry and trihrsty bar

---
+++ Tundra/Assets/Scripts/ThirstyBar.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.Rendering;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThirstyBar : MonoBehaviour
+{
+    public Slider thirstybar;
+    public float thirsty;
+    float maxThirsty = 100f;
+
+    
+    void Start()
+    {
+        thirsty = maxThirsty;
+    }
+
+    
+    void Update()
+    {
+        Debug.Log(thirsty);
+       thirstybar.value = thirsty;
+        thirsty= 1f * Time.deltaTime;
+
+
+        if (Input.GetKey(KeyCode.W))
+        { thirsty -= 2f * Time.deltaTime; }
+
+        if (Input.GetKey(KeyCode.A))
+        { thirsty -= 2f * Time.deltaTime; }
+
+        if (Input.GetKey(KeyCode.S))
+        { thirsty -= 2f * Time.deltaTime; }
+
+        if (Input.GetKey(KeyCode.D))
+        { thirsty -= 2f * Time.deltaTime; }
+
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        { thirsty -= 2.5f; }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        { thirsty -= 3f * Time.deltaTime; }
+
+
+    }
+}
--- Tundra/Assets/Scripts/hungrybarscript.cs
+++ Tundra/Assets/Scripts/hungrybarscript.cs
@@ -5,14 +5,40 @@ using UnityEngine.UI;
 public class hungrybarscript : MonoBehaviour
 {
     
+    public Slider HungerSlider;
+
+  
+
+    public float Hunger;
+    float maxHunger = 100f;
     void Start()
     {
-        
+        Hunger = maxHunger;
     }
 
     
     void Update()
     {
-        
+        Debug.Log(Hunger);
+        //deðerin oyun ýcýnde degýsecegý ýcýn buraya yazýyoruz.
+        HungerSlider.value = Hunger;
+        Hunger -= 1f* Time.deltaTime;
+
+        //açlýk barýný azalttýk
+        if(Input.GetKey(KeyCode.W))
+            { Hunger -= 2f * Time.deltaTime; }
+        if (Input.GetKey(KeyCode.A))
+        { Hunger -= 2f * Time.deltaTime; }
+        if (Input.GetKey(KeyCode.S))
+        { Hunger -= 2f * Time.deltaTime; }
+        if (Input.GetKey(KeyCode.D))
+        { Hunger -= 2f * Time.deltaTime; }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        { Hunger -= 3f * Time.deltaTime; }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        { Hunger -= 2.5f; }
+
     }
 }
